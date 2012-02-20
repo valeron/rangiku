9ed41284a8f8f8b456e8b504ce6bf21e3b2df959
Senbonzakura <senbonzakura@senbonzakura-Inspiron-N4050.(none)>
2012-02-20 09:35:22 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rangiku::Application.config.secret_token = '3762b851bcf64aa84404623e5f0057140cc568e6c408f153caa0247df58f172b9388ea8414e06a47377e387946fd30dcff5753d1c30afb347f67bb8d18ca4d7c'
